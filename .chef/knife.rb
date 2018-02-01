9e19be503486f456be4af9aad5ff251f738b8902
root <root@rleas2.mylabserver.com>
2018-02-01 17:04:19 +0000

    added motd and change apache default

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,6 +4,6 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "chefadmin"
-client_key               "#{current_dir}/chefadmin.pem"
+client_key               "~/chef-repo/.chef/chefadmin.pem"
 chef_server_url          "https://rleas3.mylabserver.com/organizations/linuxacademy"
-cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_path            ["/home/rleas/chef-repo/cookbooks"]
