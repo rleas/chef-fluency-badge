267f2b01559b4033a5ce0d746d399744e17c6944
root <root@rleas2.mylabserver.com>
2018-02-06 16:52:15 +0000

    added postgresql cookbook

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,6 +4,6 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "chefadmin"
-client_key               "~/chef-repo/.chef/chefadmin.pem"
+client_key               "/home/rleas/chef-repo/.chef/chefadmin.pem"
 chef_server_url          "https://rleas3.mylabserver.com/organizations/linuxacademy"
 cookbook_path            ["/home/rleas/chef-repo/cookbooks"]
